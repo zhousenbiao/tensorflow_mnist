22b0a943d3b96b0b459ce48edca6a35e72359473
zhousenbiao <zhousenbiao@tuandai.com>
2018-04-12 22:02:24 +0800

    knn解析

--- KNN/knn_numpy.py
+++ KNN/knn_numpy.py
@@ -1,10 +1,16 @@
 # -*- coding:utf-8 -*-
 
 
-"""
-kNN算法主要被应用于文本分类、相似推荐。
 
-KNN算法流程
+"""
+kNN(k-Nearest Neighbor)算法主要被应用于文本分类、相似推荐、面部识别、电影/音乐推荐、检测疾病等方面，
+如果一个概念很难定义,但是当你看到它的时候知道它是什么,那么kNN算法就可能是合适的方法.
+
+“k”是一个可变选项,表示任意数目的近邻.
+在选定k之后,kNN算法需要一个已经分好类的训练数据集,然后对没有分类(没有标记)的记录进行分类,
+kNN确定训练数据集中与该记录相似度"最近"的k条记录,将无标记的测试例子分配到k个近邻中占比最大的那个类别中.
+
+KNN算法流程：
 
 计算待分类点与已知类别数据集中每个点的距离；
 按照距离递增次序排序；
@@ -12,18 +18,27 @@ KNN算法流程
 确定前k个点类别出现的频率；
 返回前k个点出现频率最高的类别作为当前点的预测分类。
 
---------------------------------------
+
+
+KNN的优缺点：
+
+（1）优点：
+
+算法简单，易于实现，不需要参数估计，不需要事先训练。
+
+（2）缺点：
+
+属于懒惰算法，“平时不好好学习，考试时才临阵磨枪”，意思是kNN不用事先训练，而是在输入待分类样本时才开始运行，这一特点导致kNN计算量特别大，而且训练样本必须存储在本地，内存开销也特别大。
+-----------------------------------------------
 数值计算库NumPy库总包含两种基本的数据类型：矩阵和数组，矩阵的使用类似Matlab，本实例用得多的是数组array。
 
 1.shape()
-shape是numpy函数库中的方法，用于查看矩阵或者数组的维素
-
+shape是numpy函数库中的方法，用于查看矩阵或者数组的维数
 # >>>shape(array) 若矩阵有m行n列，则返回(m,n)
 # >>>array.shape[0] 返回矩阵的行数m，参数为1的话返回列数n
 
 2.tile()
 tile是numpy函数库中的方法，用法如下:
-
 # >>>tile(A,(m,n))  将数组A作为元素构造出m行n列的数组
 
 3.sum()
@@ -42,18 +57,15 @@ python中的方法
 
 7.min()、max()
 numpy中有min()、max()方法，用法如下
-
 # >>>array.min(0)  返回一个数组，数组中每个数都是它所在列的所有数的最小值
 # >>>array.min(1)  返回一个数组，数组中每个数都是它所在行的所有数的最小值
 
 8.listdir('str')
 python的operator中的方法
-
 # >>>strlist=listdir('str')  读取目录str下的所有文件名，返回一个字符串列表
 
 9.split()
 python中的方法，切片函数
-
 # >>>string.split('str')以字符str为分隔符切片，返回list
 
 """
@@ -75,15 +87,16 @@ parms:
 return:该测试数据的类别
 """
 def classify(inputPoint, dataSet, labels, k):
-    # 获取已知的分类的数据集（训练集）的行数
+    # 获取已知的分类的数据集（训练集）的行数，，即样本个数。shape(array) 若矩阵有m行n列，则返回(m,n)
     dataSetSize = dataSet.shape[0]
-    # tile函数讲输入点拓展成与训练集相同维数的矩阵，再计算欧式距离
+    # tile(A, (m, n)) 将数组A作为元素构造出m行n列的数组
+    # tile函数将输入点拓展成与训练集相同维数的矩阵，再计算欧式距离
     # 样本与训练集的差值矩阵
     diffMat = tile(inputPoint, (dataSetSize, 1)) - dataSet
 
     # sqDiffMat的数据类型是nump提供的ndarray，这不是矩阵的平方，而是每个元素变成原来的平方
     sqDiffMat = diffMat ** 2
-    # 计算每一行上元素的和
+    # 计算每一行上元素的和。array.sum(axis=1)按行累加，axis=0为按列累加
     sqDistances = sqDiffMat.sum(axis=1)
     # 开方得到欧拉距离矩阵
     distances = sqDistances ** 0.5
@@ -94,13 +107,14 @@ def classify(inputPoint, dataSet, labels, k):
     # 选择距离最小的k个点
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
-        # 类别数加1
+        # 类别数加1，get(key,x)从字典中获取key对应的value，没有key的话返回0
         classCount[voteIlabel] = classCount.get(voteIlabel, 0)+1
-    print(classCount)
+    # print(classCount)
 
     # 按classCount字典的第2个元素（即类别出现的次数）从大到小排序
-    sortedClassCount = sorted(classCount.items(), key= operator.itemgetter(1), reverse= True)
-    print(sortedClassCount)
+    # sorted()函数，按照第二个元素即value的次序逆向（reverse = True）排序
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    # print(sortedClassCount)
     return sortedClassCount[0][0]
 
 """
@@ -165,7 +179,7 @@ def handwritingTest():
     hwLabels,trainingMat = trainingDataSet()
     # 获取测试集
     testFileList = listdir('testDigits')
-    print(testFileList)
+    # print(testFileList)
     # 错误数
     errorCount = 0.0
     # 测试集总样本数
